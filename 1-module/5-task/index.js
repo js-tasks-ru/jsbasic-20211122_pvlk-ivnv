6c4c41bcd42379c05536cfbf3f40b6f68ffc43e7
Pavel Ivanov <pvlk.ivnv@gmail.com>
2021-11-29 20:34:26 +0200

    1-module

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,13 +1,9 @@
+
 function truncate(str, maxlength) {
-  
-  function truncate(str, maxlength) {
-  
-     str.slice(0, maxlength) + "…" ;
-      
-    }
-     truncate('Вот, что мне хотелось бы сказать на эту тему:', 19);
-    
-    
-
-
+  if ( str.length > maxlength) {
+    return str.slice(0, maxlength - 1) + "…";
+  }  else {
+    return str;
+   } 
 }
+ 
