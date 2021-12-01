48c35dd72fa1a9fff4e2a345b6705eb10e53d742
Pavel Ivanov <pvlk.ivnv@gmail.com>
2021-12-01 19:58:57 +0200

    понял и переделал готовое решение

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(salaries) 
+{
+
+    let sumOfSalaries = 0;
+    for (let key in salaries)
+    {
+        let isSalary = typeof salaries[key];
+        if (isSalary == "number" && Number.isFinite(salaries[key]))
+        {
+            sumOfSalaries += salaries[key];
+        }
+    }
+    return sumOfSalaries;
 }
+sumSalary(salaries);
